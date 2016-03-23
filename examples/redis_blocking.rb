83129cfce29e603524bbad4efe4be292d39b461d
byelipk <byelipk@gmail.com>
2016-03-23 09:04:49 -0400

    add redis blocking example

---
+++ examples/redis_blocking.rb
@@ -0,0 +1,54 @@
+require 'minitest/autorun'
+require 'pathname'
+
+lib = Pathname.new(__FILE__).parent.parent.join('lib')
+$LOAD_PATH << lib
+
+require 'redis'
+require 'sprout/reactor'
+
+class RedisBlockingHandler
+  attr_reader :client
+
+  def initialize(client)
+    @client = client
+  end
+
+  def handle
+    # When we read the data the client connection
+    # has sent us...
+    client.on(:data) do |data|
+      timeout  = 3
+      redis    = Redis.new
+
+      begin
+        # NOTE
+        # If there is data in the redis list then
+        # blpop will return straight away, otherwise
+        # it will block for 3 seconds.
+        #
+        # rpush hello the world is good
+        response = redis.blpop(data.strip, timeout)
+        write(response)
+
+      rescue Redis::CommandError => e
+        write(e.message)
+      end
+    end
+  end
+
+  def write(data)
+    client.push(data.to_s)
+    client.handle_write
+  end
+end
+
+reactor = Sprout::Reactor.new
+server  = reactor.listen '127.0.0.1', 3000
+
+server.on(:accept) do |client|
+  handler = RedisBlockingHandler.new(client)
+  handler.handle
+end
+
+reactor.start
